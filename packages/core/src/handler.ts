9a1acfc88e75478fc81876ae8a02ae4f3dab479b
Christine Panus <christine.panus@panusventures.com>
2023-06-08 14:14:13 -0500

    Adding the API

---
+++ packages/core/src/handler.ts
@@ -0,0 +1,26 @@
+import { Context, APIGatewayEvent } from 'aws-lambda';
+
+export default function handler(lambda: Function) {
+  return async function (event: APIGatewayEvent, context: Context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (error) {
+      statusCode = 500;
+      if (error instanceof Error) {
+        body = { error: error.message };
+      } else {
+        body = { error: String(error) };
+      }
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
